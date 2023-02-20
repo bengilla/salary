fe5204799a0f17338258734888bd00ae97d14523
bengilla <bengilla@outlook.com>
2023-02-20 16:55:23 +0800

    update dockerfile

--- app/models/mongo.py
+++ app/models/mongo.py
@@ -7,8 +7,8 @@ from pymongo import MongoClient
 class MongoDB:
     def __init__(self) -> None:
         # Local Testing MongoDB-------------------------------
-        # client = MongoClient(os.environ["DB_LOCAL"])
-        client = MongoClient(settings.DB_URL)
+        client = MongoClient(settings.DB_LOCAL)
+        # client = MongoClient(settings.DB_URL)
 
         self.user_info = client["USER_INFO"]
         self.user_data = client["USER_DATA"]
