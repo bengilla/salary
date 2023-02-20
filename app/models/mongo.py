5752c1252af2d54488eb3f662f45701d5f9358e6
bengilla <bengilla@outlook.com>
2023-02-20 10:58:25 +0800

    update again mongodb

--- app/models/mongo.py
+++ app/models/mongo.py
@@ -8,7 +8,7 @@ class MongoDB:
     def __init__(self) -> None:
         # Local Testing MongoDB-------------------------------
         # client = MongoClient(os.environ["DB_LOCAL"])
-        client = MongoClient(os.getenv(settings.DB_URL))
+        client = MongoClient(settings.DB_URL)
 
         self.user_info = client["USER_INFO"]
         self.user_data = client["USER_DATA"]
